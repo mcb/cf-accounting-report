77558520fcfa6c55b2bc09f5345d749348cd3bf7
Michael C. Beck <michael.beck@qualeapps.com>
2018-08-31 15:42:49 +0200

    streamlined some funcs

--- cmd/accounting-report/accounting-report.go
+++ cmd/accounting-report/accounting-report.go
@@ -15,11 +15,9 @@ import (
   "code.cloudfoundry.org/cli/plugin"
 )
 
-// BasicPlugin is the struct implementing the interface defined by the core CLI. It can
-// be found at  "code.cloudfoundry.org/cli/plugin/plugin.go"
+
 type accountingReport struct{}
 
-// simpleClient is a simple CloudFoundry client
 type apiClient struct {
   // API url, ie "https://api.system.example.com"
   API string
@@ -28,41 +26,49 @@ type apiClient struct {
   Authorization string
 }
 
-type report struct {
-  Year      int   `json:"year"`
-  Month     int   `json:"month"`
+type application struct {
+  Year      int       `json:"year"`
+  Month     int       `json:"month"`
   Average   float32   `json:"average_app_instances"`
   Maximum   float32   `json:"maximum_app_instances"`
   Hours     float32   `json:"app_instance_hours"`
 }
 
+type service struct {
+  Year      int       `json:"year"`
+  Month     int       `json:"month"`
+  Average   float32   `json:"average_instances"`
+  Maximum   float32   `json:"maximum_instances"`
+  Hours     float32   `json:"duration_in_hours"`
+}
 
-func (c *accountingReport) GetAccountUsage(client *apiClient, out io.Writer, outputJSON bool) error {
-  var res struct {
-      ReportTime  string     `json:"report_time"`
-      Monthly     []report   `json:"monthly_reports"`
-      Yearly      []report   `json:"yearly_reports"`
+func (c *accountingReport) GetAppUsage(client *apiClient, out io.Writer, outputJSON bool) error {
+
+  var appReport struct {
+      ReportTime  string          `json:"report_time"`
+      Monthly     []application   `json:"monthly_reports"`
+      Yearly      []application   `json:"yearly_reports"`
     }
 
-  err := client.Get("/system_report/app_usages", &res)
+  err := client.Get("/system_report/app_usages", &appReport)
   
   if err != nil {
     return err
   }
 
   if outputJSON {
-    return json.NewEncoder(out).Encode(res)
+    return json.NewEncoder(out).Encode(appReport)
   }
 
   table := tablewriter.NewWriter(out)
   table.SetRowLine(true)
   table.SetHeader([]string{"Type", "Year", "Month", "Average", "Maximum", "Hours"})
 
-  for _, yearly := range res.Yearly {
+  for _, yearly := range appReport.Yearly {
     table.Append([]string{"AI", fmt.Sprint(yearly.Year), "all", fmt.Sprint(yearly.Average), fmt.Sprint(yearly.Maximum), fmt.Sprint(yearly.Hours)})
   }
   
-  for _, monthly := range res.Monthly {
+  for _, monthly := range appReport.Monthly {
     table.Append([]string{"AI", fmt.Sprint(monthly.Year), fmt.Sprint(monthly.Month), fmt.Sprint(monthly.Average), fmt.Sprint(monthly.Maximum), fmt.Sprint(monthly.Hours)})
   }
   table.Render()
@@ -114,16 +120,6 @@ func newApiClient(cliConnection plugin.CliConnection) (*apiClient, error) {
 
 // Run must be implemented by any plugin because it is part of the
 // plugin interface defined by the core CLI.
-//
-// Run(....) is the entry point when the core CLI is invoking a command defined
-// by a plugin. The first parameter, plugin.CliConnection, is a struct that can
-// be used to invoke cli commands. The second paramter, args, is a slice of
-// strings. args[0] will be the name of the command, and will be followed by
-// any additional arguments a cli user typed in.
-//
-// Any error handling should be handled with the plugin itself (this means printing
-// user facing errors). The CLI will exit 0 if the plugin exits 0 and will exit
-// 1 should the plugin exits nonzero.
 func (c *accountingReport) Run(cliConnection plugin.CliConnection, args []string) {
   outputJSON := false
 
@@ -141,7 +137,7 @@ func (c *accountingReport) Run(cliConnection plugin.CliConnection, args []string
 
   switch args[0] {
     case "accounting-report":
-    err := c.GetAccountUsage(client, os.Stdout, outputJSON)
+    err := c.GetAppUsage(client, os.Stdout, outputJSON)
     if err != nil {
       log.Fatal(err)
     }
@@ -150,16 +146,6 @@ func (c *accountingReport) Run(cliConnection plugin.CliConnection, args []string
 
 // GetMetadata must be implemented as part of the plugin interface
 // defined by the core CLI.
-//
-// GetMetadata() returns a PluginMetadata struct. The first field, Name,
-// determines the name of the plugin which should generally be without spaces.
-// If there are spaces in the name a user will need to properly quote the name
-// during uninstall otherwise the name will be treated as seperate arguments.
-// The second value is a slice of Command structs. Our slice only contains one
-// Command Struct, but could contain any number of them. The first field Name
-// defines the command `cf basic-plugin-command` once installed into the CLI. The
-// second field, HelpText, is used by the core CLI to display help information
-// to the user in the core commands `cf help`, `cf`, or `cf -h`.
 func (c *accountingReport) GetMetadata() plugin.PluginMetadata {
   return plugin.PluginMetadata{
     Name: "accounting-report",
@@ -181,7 +167,7 @@ func (c *accountingReport) GetMetadata() plugin.PluginMetadata {
         // UsageDetails is optional
         // It is used to show help of usage of each command
         UsageDetails: plugin.Usage{
-          Usage: "basic-plugin-command\n   cf basic-plugin-command",
+          Usage: "cf accounting-report",
           Options: map[string]string{
             "output-json": "if set prints JSON to stdout instead of a rendered table",
             "type":        "if set only prints set type (not implemented yet)",
@@ -197,15 +183,5 @@ func (c *accountingReport) GetMetadata() plugin.PluginMetadata {
 // process, as well as any dependencies you might require for your
 // plugin.
 func main() {
-  // Any initialization for your plugin can be handled here
-  //
-  // Note: to run the plugin.Start method, we pass in a pointer to the struct
-  // implementing the interface defined at "code.cloudfoundry.org/cli/plugin/plugin.go"
-  //
-  // Note: The plugin's main() method is invoked at install time to collect
-  // metadata. The plugin will exit 0 and the Run([]string) method will not be
-  // invoked.
   plugin.Start(new(accountingReport))
-  // Plugin code should be written in the Run([]string) method,
-  // ensuring the plugin environment is bootstrapped.
 }
